19575d4c5ba64ebb2fe6fe65b2e72a2f778c8a05
Emil Hytting <emilmm21@gmail.com>
2025-07-31 21:21:32 +0200

    Push #2 - A lot of new files.

--- ConsoleUI_EMH/Program.cs
+++ ConsoleUI_EMH/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleUI_EMH;
+using System;
+
+Console.Clear();
+
+ColumnContainer column = new();
+column.AddChild(new Label("ID Navn"));
+column.AddChild(new Label("1 - Konrad Sommer"));
+column.AddChild(new Label("2 - Anne Dam"));
+column.AddChild(new Label("3 - John Doe"));
+column.AddChild(new Label("4 - Hans Hansen"));
+column.AddChild(new Button("Save"));
+
+column.Render();
+
+Console.WriteLine("\nKlik på en tast for at afslutte...");
+Console.ReadKey();
