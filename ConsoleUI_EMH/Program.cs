86712106c88e5195412f8104f818742f9fc6f039
Emil Hytting <129584561+EmilHytting@users.noreply.github.com>
2025-07-31 23:27:34 +0200

    Tried to make a launch.json dont know it is right..  still  in progress.

--- ConsoleUI_EMH/Program.cs
+++ ConsoleUI_EMH/Program.cs
@@ -1,14 +1,15 @@
 ﻿using ConsoleUI_EMH;
 using System;
 
-Console.Clear();
-
 ColumnContainer column = new();
-column.AddChild(new Label("ID Navn"));
-column.AddChild(new Label("1 - Konrad Sommer"));
-column.AddChild(new Label("2 - Anne Dam"));
-column.AddChild(new Label("3 - John Doe"));
-column.AddChild(new Label("4 - Hans Hansen"));
+column.AddChild(new Label("| ------------------- |"));
+column.AddChild(new Label("| ID |  Navn          |"));
+column.AddChild(new Label("| 1  |  Konrad Sommer |"));
+column.AddChild(new Label("| 2  |  Anne Dam      |"));
+column.AddChild(new Label("| 3  |  John Doe      |"));
+column.AddChild(new Label("| 4  |  Hans Hansen   |"));
+column.AddChild(new Label("| ------------------- |"));
+column.AddChild(new Label(""));
 column.AddChild(new Button("Save"));
 
 column.Render();
